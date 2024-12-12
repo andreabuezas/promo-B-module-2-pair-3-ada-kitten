1058de0b655df67d056f131277d7558a7b381a55
Patricia <patriciaparreirasainz@gmail.com>
2024-12-12 11:00:30 +0100

    varibles para info de gatos y colocar en mayúsculas el nombre

--- js/main.js
+++ js/main.js
@@ -1,41 +1,46 @@
 "use strict" ;
 
-// meter tarjetas gatos
+// Meter tarjetas gatos con variables
+
+const kittenOneImg = "https://dev.adalab.es/gato-siames.webp";
+const kittenOneName = "Anastacio";
+const kittenOneBreed = "Siamés";
+const kittenOneDescription = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+
+const kittenTwoImg = "https://dev.adalab.es/sphynx-gato.webp";
+const kittenTwoName = "Fiona";
+const kittenTwoBreed = "Sphynx";
+const kittenTwoDescription = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+
+const kittenThreeImg = "https://dev.adalab.es/maine-coon-cat.webp";
+const kittenThreeName = "Cielo";
+const kittenThreeBreed = "Maine Coon";
+const kittenThreeDescription = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
 
 const kittenOne = `<li class="card kittenOne">
 <article>
-    <img class="card_img" src="https://dev.adalab.es/gato-siames.webp" alt="siames-cat" />
-    <h3 class="card_title">Anastacio</h3>
-    <h4 class="card_race">Siamés</h4>
-    <p class="card_description">
-    Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.
-    </p>
+    <img class="card_img" src=${kittenOneImg} alt="siames-cat" />
+    <h3 class="card_title">${kittenOneName.toUpperCase()}</h3>
+    <h4 class="card_race1">${kittenOneBreed}</h4>
+    <p class="card_description"> ${kittenOneDescription}</p>
 </article> 
 </li>`;
 
 const kittenTwo =`<li class="card kittenTwo">
 <article>
-<img class="card_img" src="https://dev.adalab.es/sphynx-gato.webp" alt="sphynx-cat" />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">Sphynx</h4>
-<p class="card_description">
-Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-hasta con pinta de alienígena han llegado a definir a esta raza
-gatuna que se caracteriza por la «ausencia» de pelo.
-</p>
+<img class="card_img" src=${kittenTwoImg} alt="sphynx-cat" />
+<h3 class="card_title">${kittenTwoName.toUpperCase()}</h3>
+<h4 class="card_race2">${kittenTwoBreed}</h4>
+<p class="card_description">${kittenTwoDescription}</p>
 </article> 
 </li>`;
 
 const kittenThree =`<li class="card kittenThree">
 <article>
-<img class="card_img" src="https://dev.adalab.es/maine-coon-cat.webp" alt="maine-coon-cat" />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">Maine Coon</h4>
-<p class="card_description">
-Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-bella mirada se ha convertido en una de sus señas de identidad.
-Sus ojos son grandes y las orejas resultan largas y en punta.
-</p>
+<img class="card_img" src=${kittenThreeImg} alt="maine-coon-cat" />
+<h3 class="card_title">${kittenThreeName.toUpperCase()}</h3>
+<h4 class="card_race3">${kittenThreeBreed}</h4>
+<p class="card_description">${kittenThreeDescription}</p>
 </article>
 </li>`;
 
@@ -44,7 +49,7 @@ const list = document.querySelector(".js-list") ;
 list.innerHTML = kittenOne + kittenTwo + kittenThree;
 
 
-// mostrar y ocultar formulario
+// Mostrar y ocultar formulario
 const button = document.querySelector(".js-btn-add");
 const newForm = document.querySelector(".new-form");
 
@@ -53,8 +58,7 @@ button.addEventListener('click', ()=>{
 });
 
 
-// ocultar la sección del formulario al hacer clic en el botón cancelar
-// vaciar inputs al hacer clic en cancelar
+// Ocultar la sección del formulario al hacer clic en el botón cancelar + Vaciar inputs al hacer clic en cancelar
 
 const cancel = document.querySelector(".button-cancel");
 const form = document.querySelector(".form");
@@ -68,19 +72,12 @@ cancel.addEventListener('click', ()=>{
 });
 
 
-//FILTRAR: Búsqueda por descripción
+// FILTRAR: Búsqueda por descripción
 const searchButton = document.querySelector(".js_button-search");
 const descriptionField = document.querySelector(".js_in_search_desc");
-//const description = descriptionField.value;
 
-// searchButton.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     const description = descriptionField.value;
-//     console.log(description);
-//   });
 
-  
-// acceder al texto de la descripción de cada gatito
+// Acceder al texto de la descripción de cada gatito
 const kittenOneCard = document.querySelector(".kittenOne");
 const kittenDesc1 = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
 
@@ -90,15 +87,6 @@ const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, e
 const kittenThreeCard = document.querySelector(".kittenThree");
 const kittenDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
 
-// searchButton.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     const description = descriptionField.value;
-//     if (kittenDesc1.includes(description)) {
-//         kittenTwoCard.classList.add('collapsed');
-//         kittenThreeCard.classList.add('collapsed');
-//       }
-//     console.log(description);
-//   });
 
 searchButton.addEventListener("click", (event) => {
     event.preventDefault();
@@ -113,7 +101,7 @@ searchButton.addEventListener("click", (event) => {
     event.preventDefault();
     const description = descriptionField.value;
     if (kittenDesc2.includes(description)) {
-        list.innerHTML = kittenTwo;
+        list.innerHTML += kittenTwo;
       }
     console.log(description);
   });
@@ -122,7 +110,11 @@ searchButton.addEventListener("click", (event) => {
     event.preventDefault();
     const description = descriptionField.value;
     if (kittenDesc3.includes(description)) {
-        list.innerHTML = kittenThree;
+        list.innerHTML += kittenThree;
       }
     console.log(description);
   });
+
+
+// 2. LISTADO - Bonus: ¿Y si no hay raza? 
+
