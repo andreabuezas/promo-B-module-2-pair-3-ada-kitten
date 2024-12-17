a013453c713e541cce6ef9e14471b96c1c88d64f
Patricia <patriciaparreirasainz@gmail.com>
2024-12-17 10:59:47 +0100

    ejercicios funciones 2 y objetos

--- js/main.js
+++ js/main.js
@@ -1,76 +1,74 @@
 "use strict" ;
 
 // METER TARJETAS DE LOS GATOS CON VARIABLES Y FUNCIONES
+// LISTADO: CONVERTIR CADA GATITO EN UN OBJETO
+
+// Declarar cada gato como objetos con propiedades
+const kittenData_1 = {
+  image: 'https://dev.adalab.es/gato-siames.webp',
+  name: 'Anastacio',
+  desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
+  race: 'Siamés',
+};
+
+const kittenData_2 = {
+  image: 'https://dev.adalab.es/sphynx-gato.webp',
+  name: 'Fiona',
+  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
+  race: 'Sphynx',
+};
+
+const kittenData_3 = {
+  image: 'https://dev.adalab.es/maine-coon-cat.webp',
+  name: 'Cielo',
+  desc: 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
+  race: 'Maine Coon',
+};
+
 // Declarar los campos del gato 1
-const kittenOneImg = "https://dev.adalab.es/gato-siames.webp";
-const kittenOneName = "Anastacio";
-const kittenOneBreed = "Siamés";
-const kittenOneDescription = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
+// const kittenOneImg = "https://dev.adalab.es/gato-siames.webp";
+// const kittenOneName = "Anastacio";
+// const kittenOneBreed = "Siamés";
+// const kittenOneDescription = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
 
-// Declarar los campos del gato 2
-const kittenTwoImg = "https://dev.adalab.es/sphynx-gato.webp";
-const kittenTwoName = "Fiona";
-const kittenTwoBreed = "Sphynx";
-const kittenTwoDescription = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
+// // Declarar los campos del gato 2
+// const kittenTwoImg = "https://dev.adalab.es/sphynx-gato.webp";
+// const kittenTwoName = "Fiona";
+// const kittenTwoBreed = "Sphynx";
+// const kittenTwoDescription = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
 
-// Declarar los campos del gato 3
-const kittenThreeImg = "https://dev.adalab.es/maine-coon-cat.webp";
-const kittenThreeName = "Cielo";
-const kittenThreeBreed = "Maine Coon";
-const kittenThreeDescription = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
+// // Declarar los campos del gato 3
+// const kittenThreeImg = "https://dev.adalab.es/maine-coon-cat.webp";
+// const kittenThreeName = "Cielo";
+// const kittenThreeBreed = "Maine Coon";
+// const kittenThreeDescription = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
 
-
-// const kittenOne = `<li class="card kittenOne">
-// <article>
-//     <img class="card_img" src=${kittenOneImg} alt="siames-cat" />
-//     <h3 class="card_title">${kittenOneName.toUpperCase()}</h3>
-//     <h4 class="card_race1">${kittenOneBreed}</h4>
-//     <p class="card_description"> ${kittenOneDescription}</p>
-// </article> 
-// </li>`;
-
-// const kittenTwo =`<li class="card kittenTwo">
-// <article>
-// <img class="card_img" src=${kittenTwoImg} alt="sphynx-cat" />
-// <h3 class="card_title">${kittenTwoName.toUpperCase()}</h3>
-// <h4 class="card_race2">${kittenTwoBreed}</h4>
-// <p class="card_description">${kittenTwoDescription}</p>
-// </article> 
-// </li>`;
-
-// const kittenThree =`<li class="card kittenThree">
-// <article>
-// <img class="card_img" src=${kittenThreeImg} alt="maine-coon-cat" />
-// <h3 class="card_title">${kittenThreeName.toUpperCase()}</h3>
-// <h4 class="card_race3">${kittenThreeBreed}</h4>
-// <p class="card_description">${kittenThreeDescription}</p>
-// </article>
-// </li>`;
 
 // Declarar la función de renderizar tarjeta de gato con los 4 parámetros
 function renderKitten (img, name, breed, description) {
-  return `<li class="card kittenOne">
+  return `<li class="card kitten">
     <article>
     <img class="card_img" src=${img} alt="siames-cat" />
     <h3 class="card_title">${name.toUpperCase()}</h3>
-    <h4 class="card_race1">${breed}</h4>
+    <h4 class="card_race">${breed}</h4>
     <p class="card_description"> ${description}</p>
     </article> 
     </li>`;
 }
 
 // Declarar las variables con la función de renderizar para cada uno de los tres gatitos
-const kittenOne = renderKitten(kittenOneImg, kittenOneName, kittenOneBreed, kittenOneDescription);
+const kittenOne = renderKitten(kittenData_1.image, kittenData_1.name, kittenData_1.race, kittenData_1.desc);
 
-const kittenTwo = renderKitten(kittenTwoImg, kittenTwoName, kittenTwoBreed, kittenTwoDescription);
+const kittenTwo = renderKitten(kittenData_2.image, kittenData_2.name, kittenData_2.race, kittenData_2.desc);
 
-const kittenThree = renderKitten(kittenThreeImg, kittenThreeName, kittenThreeBreed, kittenThreeDescription);
+const kittenThree = renderKitten(kittenData_3.image, kittenData_3.name, kittenData_3.race, kittenData_3.desc);
 
 // Declaramos el elemento ul del HTML que contiene la lista de gatitos
 const list = document.querySelector(".js-list") ;
 
 // Llamamos a la función de renderizar incluyendo las constantes para cada gatito
 list.innerHTML = kittenOne + kittenTwo + kittenThree;
+
 
 
 // MOSTRAR Y OCULTAR FORMULARIO CON FUNCIONES
@@ -118,42 +116,19 @@ cancel.addEventListener('click', ()=>{
 const searchButton = document.querySelector(".js_button-search");
 const descriptionField = document.querySelector(".js_in_search_desc");
 
-// Declarar la descripción existente de cada tarjeta de gatito
-const kittenOneCard = document.querySelector(".kittenOne");
-const kittenDesc1 = "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.";
-
-const kittenTwoCard = document.querySelector(".kittenTwo");
-const kittenDesc2 = "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.";
-
-const kittenThreeCard = document.querySelector(".kittenThree");
-const kittenDesc3 = "Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.";
-
-// Función para la tarjeta 1
-searchButton.addEventListener("click", (event) => {
+const filterKitten = (event) => {
   event.preventDefault();
-  const description = descriptionField.value;
-  if (kittenDesc1.includes(description)) {
-      list.innerHTML = kittenOne;
-    }
-  console.log(description);
-});
+  list.innerHTML = '';
+  const description = descriptionField.value; // esta declaración se hace dentro de la función pq si estuviera fuera, en el momento de renderizar la página estaría vacía
+  if (kittenOneDescription.includes(description)) {
+    list.innerHTML += kittenOne;
+  }
+  if (kittenTwoDescription.includes(description)) {
+    list.innerHTML += kittenTwo;
+  }
+  if (kittenThreeDescription.includes(description)) {
+    list.innerHTML += kittenThree;
+  }
+};
 
-// Función para la tarjeta 2
-searchButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const description = descriptionField.value;
-  if (kittenDesc2.includes(description)) {
-      list.innerHTML += kittenTwo;
-    }
-  console.log(description);
-});
-
-// Función para la tarjeta 3
-searchButton.addEventListener("click", (event) => {
-  event.preventDefault();
-  const description = descriptionField.value;
-  if (kittenDesc3.includes(description)) {
-      list.innerHTML += kittenThree;
-    }
-  console.log(description);
-});
+searchButton.addEventListener('click', filterKitten);
